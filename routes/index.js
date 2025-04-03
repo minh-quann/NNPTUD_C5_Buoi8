9cf6d4e033489cd8e0e4f44b3c6502b786a6a57f
minh-quann <quannguyen19092003@gmail.com>
2025-04-03 14:31:09 +0700

    update index compele slug

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,36 @@ router.get('/', function(req, res, next) {
       message:"heheeheheh"
     }
   );
-});
+}
+);
+ router.get('/slug/:category', async function(req, res) {
+    try {
+        let category = await categoryModel.findOne({ slug: req.params.category });
+        if (!category) {
+            return res.status(404).send({ success: false, message: 'Category không tồn tại' });
+        }
+        let products = await productModel.find({ category: category._id });
+        res.status(200).send({ success: true, data: products });
+    } catch (error) {
+        res.status(500).send({ success: false, message: error.message });
+    }
+  });
+  router.get('/slug/:category/:product', async function(req, res) {
+    try {
+        let category = await categoryModel.findOne({ slug: req.params.category });
+        if (!category) {
+            return res.status(404).send({ success: false, message: 'Category không tồn tại' });
+        }
+        let product = await productModel.findOne({ slug: req.params.product, category: category._id });
+        if (!product) {
+            return res.status(404).send({ success: false, message: 'Product không tồn tại' });
+        }
+        res.status(200).send({ success: true, data: product });
+    } catch (error) {
+        res.status(500).send({ success: false, message: error.message });
+    }
+  });
+
 
 
 module.exports = router;
--- routes/products.js
+++ routes/products.js
@@ -57,34 +57,7 @@ router.get('/:id', async function(req, res, next) {
     });
   }
 });
-  router.get('/slug/:category', async function(req, res) {
-    try {
-        let category = await categoryModel.findOne({ slug: req.params.category });
-        if (!category) {
-            return res.status(404).send({ success: false, message: 'Category không tồn tại' });
-        }
-        let products = await productModel.find({ category: category._id });
-        res.status(200).send({ success: true, data: products });
-    } catch (error) {
-        res.status(500).send({ success: false, message: error.message });
-    }
-  });
-  router.get('/slug/:category/:product', async function(req, res) {
-    try {
-        let category = await categoryModel.findOne({ slug: req.params.category });
-        if (!category) {
-            return res.status(404).send({ success: false, message: 'Category không tồn tại' });
-        }
-        let product = await productModel.findOne({ slug: req.params.product, category: category._id });
-        if (!product) {
-            return res.status(404).send({ success: false, message: 'Product không tồn tại' });
-        }
-        res.status(200).send({ success: true, data: product });
-    } catch (error) {
-        res.status(500).send({ success: false, message: error.message });
-    }
-  });
-
+ 
   router.post('/', async function(req, res) {
     try {
         let category = await categoryModel.findOne({ name: req.body.category });
